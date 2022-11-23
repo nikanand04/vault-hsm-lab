d2025e1825ebcff879fa870eab39746fd377885d
joshua-bradley <jb@joshuabradley.io>
2022-11-22 23:50:26 -0800

    fix tf code base

--- tf/rds.tf
+++ tf/rds.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "vault" {
   engine_version         = "5.7"
   instance_class         = "db.t3.micro"
   name                   = "mysqldb"
-  username               = "hc-admin"
+  username               = "hcadmin"
   password               = "migrateVault!"
   parameter_group_name   = "default.mysql5.7"
   vpc_security_group_ids = [aws_security_group.vault.id]
