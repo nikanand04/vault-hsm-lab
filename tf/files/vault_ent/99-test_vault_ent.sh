703d92a6c2c9bb66b0d4d2cbeb16658251f3cfb8
joshua-bradley <jb@joshuabradley.io>
2022-12-08 22:05:10 -0800

    tf code base - fix provision remote script for ent-node

--- tf/files/vault_ent/99-test_vault_ent.sh
+++ tf/files/vault_ent/99-test_vault_ent.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+readonly __DEBUG="${MP_DEBUG:=0}"
+
+# Define field seperator for word spllitting
+# IFS=$' \n\t'
+
+# set debug output
+if [[ ${__DEBUG} = 1 || ${__DEBUG} = "TRUE" ]]; then
+    set -o xtrace #Turn on traces, useful while debugging but unset by default
+fi
+
+# constants and environment
+declare -xr VAULT_ADDR="http://127.0.0.1:8200"
+
 vault login $(jq -r .root_token <~/vault_init.json)
 sleep 2
 
